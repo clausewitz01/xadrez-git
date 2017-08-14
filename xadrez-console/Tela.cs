f3375a5d9d3cae216e1e613be78440bb3a0c0490
octavio elliot <octavioelliot@gmail.com>
2017-08-14 00:31:46 -0300

    implementacao de todas as pecas

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -21,6 +21,7 @@ namespace xadrez_console {
             }
             else {
                 Console.WriteLine("XEQUEMATE!");
+                Console.WriteLine("Vencedor: " + partida.jogadorAtual);
             }
         }
 
